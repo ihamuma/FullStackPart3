83e47521e031db3782334ca3ca5fe2aa233f98cd
ihamuma <you@example.com>
2020-10-28 14:47:33 +0200

    practice practice

--- practice/index.js
+++ practice/index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -21,11 +24,41 @@ let notes = [
     }
   ]
 
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
   })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+  })
+
+  app.delete('api/notes/:id', (request, response) => {
+      const id = Number(request.params.id)
+      notes = notes.filter(note => note.id !== id)
+      response.status(204).end
+  })
+
+  app.post('/api/notes', (request, response) => {
+      const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+      const note = request.body
+      note.id = maxId + 1
+      notes = notes.concat(note)
+      response.json(note)
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
